2722431955489488010cf0aa4d5bb2361a3c613b
s0mething382 <59333057+s0mething382@users.noreply.github.com>
2021-05-26 23:59:01 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(
+  <div>
+    <h1>My favourite zombieland saga waifus</h1>
+    <ul>
+      <li>Hoshikawa Lily</li>
+      <li>Minamoto Sakura</li>
+      <li>Yugiri</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
